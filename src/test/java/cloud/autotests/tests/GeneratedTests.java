15fdf74b3a4e3b39e8c9f4f0638c88ae1952702c
Дарья Обедкина <dobedkina@solit-clouds.ru>
2021-11-02 22:38:03 +0300

    Added all tests

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -4,6 +4,8 @@ package cloud.autotests.tests;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -24,6 +26,36 @@ public class GeneratedTests extends TestBase {
         $(".header__catalog").click();
         $(".group-card").$(byText("Мясо, птица, колбаса")).click();
         $(".catalog-view__title").shouldHave(Condition.text("Мясо, птица, колбаса"));
+    }
+
+    @Test
+    public void searchItemInCatalog() {
+        open("");
+        $(".catalog-search__field").setValue("Колбаса");
+        $(".catalog-search__icon-label").shouldBe(Condition.enabled).click();
+        $(".sku-card-small-container").shouldHave(Condition.text("Колбаса"));
+    }
+
+    @Test
+    public void checkSearchSuggestions() {
+        open("");
+        $(".catalog-search__field").setValue("Колбаса");
+        $(".spinner-lock").shouldBe(Condition.visible);
+        $(".catalog-search__suggestions a").shouldHave(Condition.text("Колбаса"));
+    }
+
+    @Test
+    public void addOneProductToCart() {
+        open("");
+        $(".header__catalog").click();
+        $(".group-card").$(byText("Мясо, птица, колбаса")).click();
+        $(".sku-card-small-basket-control__default-control").click();
+        $(".sku-card-small-basket-control__input").shouldHave(Condition.value("1"));
+        $(".header-catalog-link__counter--show").shouldHave(Condition.text("1"), Duration.ofSeconds(3));
+        $(".header-catalog-link__icon").click();
+        $(".sku-list-in-basket").shouldBe(Condition.visible);
+        $(byText("Товаров в корзине")).sibling(0).shouldHave(Condition.text("1"));
+
     }
 
 }
